e6f282e00a34069b4ddf989f4c963ad079399bdb
junxiang <junxiang.oriint@gmail.com>
2019-07-24 09:49:18 +0800

    [update] 修改 pgsql 密碼

--- database/database.go
+++ database/database.go
@@ -9,7 +9,7 @@ import (
 
 // Init : Initial Db connection
 func Init() {
-	db, err := gorm.Open("postgres", "host=127.0.0.1 user=tatara dbname=tatara sslmode=disable password=000")
+	db, err := gorm.Open("postgres", "host=127.0.0.1 user=tatara dbname=tatara sslmode=disable password=0000")
 	if err != nil {
 		log.Error("database.Init", err)
 	}
